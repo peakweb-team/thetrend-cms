d62afa79566ec5fb1d6c5555d837150a1ce6ede6
marktcotton <hello@markcotton.co.uk>
2025-11-21 15:40:22 +0000

    Add glossary and directory content types with categories
    Introduces two new content type systems with independent categorization:
    - Glossary: terms, acronyms, and concepts with optional images
    - Directory: resource listings with required images
    
    Both support draft/publish workflow, dynamic content blocks, repeatable links component, and many-to-many category relations. Public read permissions configured in bootstrap.

---
+++ src/api/directory-category/controllers/directory-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * directory-category controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::directory-category.directory-category');
---
+++ src/api/directory-category/services/directory-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * directory-category service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::directory-category.directory-category');
